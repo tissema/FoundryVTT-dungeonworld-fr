1e0b6cd8703377432fc39c384bb701699a2a13cf
YanK l'Innommé <yanklinnomme@gmail.com>
2022-11-20 12:30:28 +0100

    20.11.22-v.1.0.0

--- babele-register.js
+++ babele-register.js
@@ -1,9 +1,31 @@
 Hooks.on('init', () => {
-    if(typeof Babele !== 'undefined') {
-        Babele.get().register({
-            module: 'dungeonworld-fr',
-            lang: 'fr',
-            dir: 'compendium'
-        });
+    game.settings.register('dungeonworld-fr', 'autoRegisterBabel', {
+      name: 'Activer automatiquement la traduction via Babele',
+      hint: 'Met automatiquement en place les traductions au sein de Babele sans avoir besoin de pointer vers le répertoire contenant les traductions.',
+      scope: 'world',
+      config: true,
+      default: true,
+      type: Boolean,
+      onChange: value => {
+        if (value) {
+          autoRegisterBabel();
+        }
+  
+        window.location.reload();
+      },
+    });
+  
+    if (game.settings.get('dungeonworld-fr', 'autoRegisterBabel')) {
+      autoRegisterBabel();
     }
-});
+  });
+  
+  function autoRegisterBabel() {
+    if (typeof Babele !== 'undefined') {
+      Babele.get().register({
+        module: 'dungeonworld-fr',
+        lang: 'fr',
+        dir: 'compendium',
+      });
+    }
+  }
